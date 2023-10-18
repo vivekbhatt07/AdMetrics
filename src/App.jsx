7c428759a5d2160f2a0bd82ea58dec0516859a1b
Vivek Bhatt <vivekbhatt618@gmail.com>
2023-10-18 23:27:36 +0530

    style: form

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,5 @@
 import { Routes, Route } from "react-router-dom";
-import { Dashboard, CreateAds } from "./pages";
+import { Dashboard, CreateAds, MediaAdForm, TextAdForm } from "./pages";
 import "./App.css";
 
 function App() {
@@ -8,6 +8,8 @@ function App() {
       <Routes>
         <Route path="/" element={<Dashboard />} />
         <Route path="/createAd" element={<CreateAds />} />
+        <Route path="/media" element={<MediaAdForm />} />
+        <Route path="/text" element={<TextAdForm />} />
       </Routes>
     </div>
   );
---
+++ src/components/inputs/index.js
@@ -0,0 +1,4 @@
+import BasicInput from "./BasicInput";
+import TextAreaInput from "./TextAreaInput";
+
+export { BasicInput, TextAreaInput };
