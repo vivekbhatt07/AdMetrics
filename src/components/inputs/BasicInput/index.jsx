9a94d2dc05ee0a1f1fd1ffdca0f9aef78f8a007c
Vivek Bhatt <vivekbhatt618@gmail.com>
2023-10-19 00:19:38 +0530

    style: form grid

--- src/components/inputs/BasicInput/index.jsx
+++ src/components/inputs/BasicInput/index.jsx
@@ -1,8 +1,8 @@
 import React from "react";
 
-const BasicInput = ({ labelText, placeholderText }) => {
+const BasicInput = ({ labelText, placeholderText, className }) => {
   return (
-    <div className="flex flex-col gap-2">
+    <div className={`flex flex-col gap-2 ${className}`}>
       <label>{labelText}</label>
       <input
         className="border-2 border-[#ddd] py-2 px-2 focus:outline-0 focus:border-[#0096ff] transition-all rounded-md"
--- src/pages/TextAdForm/index.jsx
+++ src/pages/TextAdForm/index.jsx
@@ -3,6 +3,7 @@ import { PrimaryContainer } from "../../components/containers";
 import { PageContainer } from "../../layout";
 import { BasicInput, TextAreaInput } from "../../components/inputs";
 import { Button } from "@mui/material";
+import "./TextAdForm.css";
 
 const TextAdForm = () => {
   return (
@@ -18,22 +19,37 @@ const TextAdForm = () => {
           height: "100%",
         }}
       >
-        <form className="flex-1">
+        <form className="textAdForm">
           <BasicInput
             labelText="Heading 01"
             placeholderText="Add a heading that would make users interested"
+            className="headOne"
           />
           <BasicInput
             labelText="Heading 02"
             placeholderText="Add a heading that would make users interested"
+            className="headTwo"
           />
           <BasicInput
             labelText="Business Name"
             placeholderText="Add your business name"
+            className="businessName"
           />
           <BasicInput
             labelText="Website URL"
             placeholderText="Add the URL of the landing page you want to redirect users to"
+            className="websiteUrl"
+          />
+          <TextAreaInput
+            labelText="Description 01"
+            placeholderText="Add primary text to help users understand more about your products, services or offers"
+            className="descriptionOne"
+            style={{ height: "100%" }}
+          />
+          <BasicInput
+            labelText="Button Label"
+            placeholderText="Select a label that best suits your ad"
+            className="buttonLabel"
           />
         </form>
         <div className="flex justify-end">
