2bc1a7345c6d070c364adf1a4b83fc7a0b1361ba
Vivek Bhatt <vivekbhatt618@gmail.com>
2023-10-19 17:16:14 +0530

    add: media form

--- src/components/inputs/TextAreaInput/index.jsx
+++ src/components/inputs/TextAreaInput/index.jsx
@@ -5,7 +5,7 @@ const TextAreaInput = ({ labelText, placeholderText, className, style }) => {
     <div className={`flex flex-col gap-2 ${className}`}>
       <label>{labelText}</label>
       <textarea
-        className="border-2 border-[#ddd] py-2 px-2 focus:outline-0 focus:border-[#0096ff] transition-all rounded-md"
+        className="border-2 border-[#ddd] py-2 px-2 focus:outline-0 focus:border-[#0096ff] transition-all rounded-md resize-none"
         placeholder={placeholderText}
         style={{ ...style }}
       />
